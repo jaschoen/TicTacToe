d9ac8a890e24e126a574958726e18114a60dc9d9
Justin Schoen <jaschoen@gmail.com>
2016-02-23 15:59:40 -0600

    add wins for 4x4 and try it out...it hangs

--- lib/tictactoe/rules.rb
+++ lib/tictactoe/rules.rb
@@ -1,13 +1,26 @@
 class Rules
 
-  TICTACTOE_WINS = [
+  # TICTACTOE_WINS = [
+  #   # Horizontal wins:
+  #   [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  #   # Vertical wins:
+  #   [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  #   # Diagonal wins:
+  #   [0, 4, 8], [2, 4, 6]
+  # ]
+
+    TICTACTOE_WINS = [
     # Horizontal wins:
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 1, 2, 3],   [4, 5, 6, 7],  [8, 9, 10, 11], [12, 13, 14, 15],
     # Vertical wins:
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8, 12],  [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15],
     # Diagonal wins:
-    [0, 4, 8], [2, 4, 6]
+    [0, 5, 10, 15], [3, 6, 9, 12]
   ]
+  # 0  1  2  3  
+  # 4  5  6  7
+  # 8  9  10 11
+  # 12 13 14 15
   
 
   # def possible_wins(size)
@@ -29,7 +42,7 @@ class Rules
     # 2xsize..3*size -1
 
   def self.size
-    3
+    4
   end
 
   # Return winning combo if are all the same, and not nil, else return false
